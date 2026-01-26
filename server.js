591111e447d05e076fee8cfeecfdbcf99f38100d
Perspicacious <johnayomide920@gmail.com>
2026-01-26 15:32:44 +0100

    'u'

--- server.js
+++ server.js
@@ -81,12 +81,12 @@ app.get("/health", async (req, res) => {
   }
 });
 
-if (process.env.NODE_ENV !== 'production') {
-  const PORT = process.env.PORT || 5000;
-  app.listen(PORT, () => {
-    console.log(`🚀 Server running on http://localhost:${PORT}`);
-    connectDB();
-  });
-}
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`🚀 Server running on port ${PORT}`);
+  connectDB();
+});
+
 
 module.exports = app;
