de0aa6470736b52149363d92377fb2f31e222efc
Perspicacious <johnayomide920@gmail.com>
2026-07-26 14:41:17 +0100

    ne

--- routes/mailRoutes.js
+++ routes/mailRoutes.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router();
 const mailController = require('../controllers/mailController');
 
-router.post('/send', mailController.sendEmail);
 router.post('/send-batch', mailController.sendBatch);
 router.get('/stats', mailController.getStats);
 router.get('/history', mailController.getHistory);
